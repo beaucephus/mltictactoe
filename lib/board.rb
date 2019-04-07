4d15572cec9ef0f6f5130bfca9bde432b09a720c
Joseph Browning <joseph.browning@macys.com>
2019-04-07 13:00:25 -0400

    Finish board class. Time to use it!

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,5 @@
 # Board
 class Board
-  attr_accessor(:tiles)
-
   def initialize
     @tiles = new_board
   end
@@ -9,13 +7,58 @@ class Board
   def new_board
     tile_hash = {}
 
-    (0..2).each do |row|
-      (0..2).each do |column|
-        tile_hash[[row, column]] = ""
+    %w[a b c].each do |file|
+      %w[1 2 3].each do |rank|
+        tile_hash["#{file}#{rank}"] = ""
       end
     end
 
     tile_hash
+  end
+
+  def state
+    state = ""
+    %w[a b c].each do |file|
+      %w[3 2 1].each do |rank|
+        state += "#{tile(file, rank)}_"
+      end
+    end
+
+    state[0, state.length - 1]
+  end
+
+  def to_s
+    %w[3 2 1].each do |rank|
+      puts "#{formatted_tile('a', rank)}|#{formatted_tile('b', rank)}|#{formatted_tile('c', rank)}"
+    end
+
+    nil
+  end
+
+  def formatted_tile(file, rank)
+    return " " if tile(file, rank).empty?
+
+    tile(file, rank)
+  end
+
+  def play_token_on_tile(token, file, rank)
+    @tiles["#{file}#{rank}"] = token.to_s
+  end
+
+  def tile(file, rank)
+    @tiles["#{file}#{rank}"]
+  end
+
+  def tiles_in_file(file)
+    # TODO
+  end
+
+  def tiles_in_rank(rank)
+    # TODO
+  end
+
+  def tiles_in_diagonal(file, rank)
+    # TODO
   end
 
   def full?
@@ -24,7 +67,7 @@ class Board
     true
   end
 
-  def update_all(token)
+  def update_all_tiles(token)
     @tiles.each_key do |k|
       @tiles[k] = token
     end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,17 +2,17 @@ require_relative "spec_helper.rb"
 require_relative "../lib/board.rb"
 
 describe Board do
-  before(:all) do
+  before(:each) do
     @test_board = Board.new
   end
 
   describe("#new_board") do
     it "returns a hash of tile positions" do
-      expect(@test_board.tiles).to be_instance_of(Hash)
+      expect(@test_board.new_board).to be_instance_of(Hash)
     end
 
     it "returns a board of empty tiles" do
-      @test_board.tiles.each_value do |tile|
+      @test_board.new_board.each_value do |tile|
         expect(tile).to eq("")
       end
     end
@@ -24,8 +24,34 @@ describe Board do
     end
 
     it "returns true with a full board" do
-      @test_board.update_all("x")
+      @test_board.update_all_tiles("x")
       expect(@test_board.full?).to eq(true)
+    end
+  end
+
+  describe("#play_token_on_tile") do
+    before do
+      @test_board.play_token_on_tile("x", "a", "1")
+    end
+
+    it "sets a tile with the specified token" do
+      expect(@test_board.instance_variable_get(:@tiles)["a1"]).to eq("x")
+    end
+  end
+
+  describe("#tile") do
+    before do
+      @test_board.play_token_on_tile("x", "a", "1")
+    end
+
+    it "gets a tile with the specified position" do
+      expect(@test_board.tile("a", "1")).to eq("x")
+    end
+  end
+
+  describe("#state") do
+    it "returns board state string" do
+      expect(@test_board.state).to eq("________")
     end
   end
 end
